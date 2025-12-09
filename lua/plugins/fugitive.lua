1eb78ecb03fe6443edd2c52235312ddd4c0d39ca
Boris Dibon <boris.dibon@gmail.com>
2025-12-09 23:25:45 +0100

    add vim-rhubarb for gh interactions

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,12 @@
 return {
   "tpope/vim-fugitive",
+  dependencies = {
+    "tpope/vim-rhubarb"
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>go", "<Cmd>GBrowse<CR>", { desc = "[G]it [O]pen in browser" })
+    vim.keymap.set("v", "<leader>go", "<Esc><Cmd>'<,'>GBrowse<CR>", { desc = "[G]it [O]pen in browser" })
+    vim.keymap.set("v", "<leader>gy", "<Cmd>GBrowse!<CR>", { desc = "[G]it [Y]ank link" })
+    vim.keymap.set("n", "<leader>gy", "<Cmd>GBrowse!<CR>", { desc = "[G]it [Y]ank link" })
+  end
 }
