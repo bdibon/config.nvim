c1ce17ea5646035e495e3d729402b34c105fed2f
Boris Dibon <boris.dibon@gmail.com>
2026-01-15 20:40:17 +0100

    improve picker for workspace symbols

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -101,7 +101,15 @@ local M = {
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+        map('gW',
+          function()
+            require('telescope.builtin').lsp_dynamic_workspace_symbols({
+              path_display = { "smart" },
+              layout_strategy = 'vertical',
+            })
+          end,
+          'Open Workspace Symbols'
+        )
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
